e057ad897de8709360c6a93e40263f0626288af3
adrianhouston <adrianhouston@proton.me>
2025-12-27 01:20:01 +0800

    Create ini.sh

---
+++ termux/ini.sh
@@ -0,0 +1,11 @@
+curl --output-dir /sdcard/Download/ -O https://github.com/adrianhouston/setup/raw/refs/heads/main/termux/setup.sh
+curl --output-dir /sdcard/Download/ -O https://github.com/adrianhouston/setup/raw/refs/heads/main/termux/setup.fish
+
+chmod +x /sdcard/Download/setup.sh
+/sdcard/Download/setup.sh
+
+chmod +x /sdcard/Download/setup.sh
+/sdcard/Download/setup.sh
+
+chmod +x /sdcard/Download/setup.fish
+/sdcard/Download/setup.fish
