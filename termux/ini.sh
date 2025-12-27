74832882a1213357e357bd79a5a2be6015560dc9
adrianhouston <adrianhouston@proton.me>
2025-12-27 13:39:50 +0800

    Update ini.sh

--- termux/ini.sh
+++ termux/ini.sh
@@ -1,11 +1,8 @@
-curl --output-dir /sdcard/Download/ -O https://github.com/adrianhouston/setup/raw/refs/heads/main/termux/setup.sh
-curl --output-dir /sdcard/Download/ -O https://github.com/adrianhouston/setup/raw/refs/heads/main/termux/setup.fish
+# curl --output-dir /sdcard/Download/ -O https://github.com/adrianhouston/setup/raw/refs/heads/main/termux/setup.sh
+# curl --output-dir /sdcard/Download/ -O https://github.com/adrianhouston/setup/raw/refs/heads/main/termux/setup.fish
 
-chmod +x /sdcard/Download/setup.sh
-/sdcard/Download/setup.sh
+bash /sdcard/Download/setup.sh
 
-chmod +x /sdcard/Download/setup.sh
-/sdcard/Download/setup.sh
+bash /sdcard/Download/setup.sh
 
-chmod +x /sdcard/Download/setup.fish
-/sdcard/Download/setup.fish
+fish /sdcard/Download/setup.fish
